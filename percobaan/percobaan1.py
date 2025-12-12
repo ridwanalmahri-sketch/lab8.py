3867b8aa051c21016c3f9e184b022e61c40fecf8
WAN. <ridwanalmahri@gmail.com>
2025-12-12 20:27:08 +0700

    percobaan

---
+++ percobaan/percobaan1.py
@@ -0,0 +1,4 @@
+print('Silahkan masukan dua angka untuk pembagian.')
+num1 = int(input('masukan angka yang akan dibagi: '))
+num2 = int(input('masukan angka pembagi:'))
+print('{0} dibagi {1} = {2}'.format(num1, num2, num1/num2))
